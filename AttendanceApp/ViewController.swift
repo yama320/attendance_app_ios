5d651cf2b62e79fc1b3c1db044bcdc95ec7cf35c
山崎光男 <mitsuo@MacBook-Pro2016.local>
2017-06-05 01:53:00 +0900

    [ticket: #10][task] Xcodeプロジェクトの作成

---
+++ AttendanceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AttendanceApp
+//
+//  Created by 山崎光男 on 2017/06/05.
+//  Copyright © 2017 Mitsuo Yamazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
